353fabb672f18aa99c640db45feb06e53cb750d2
Kamran Kiani <kiani.km@gmail.com>
2025-09-30 00:53:52 +0330

    Update styles in DetailsCard and WeatherSection
    Removed unnecessary margin class from DetailsCard value element and replaced max-height with 'contain-size' class in WeatherSection for improved layout control.

--- src/components/DetailsCard/DetailsCard.tsx
+++ src/components/DetailsCard/DetailsCard.tsx
@@ -10,7 +10,7 @@ function DetailsCard({
   return (
     <div className="flex flex-col items-start  gap-6 rounded-12 p-5 xl:py-5 xl:px-2 pe-0 bg-neutral-800 border border-neutral-600">
       <dt className="text-neutral-200 text-500 font-medium">{label}</dt>
-      <dd className="text-600 xl:text-800 font-light xl:-ms-2">{value}</dd>
+      <dd className="text-600 xl:text-800 font-light">{value}</dd>
     </div>
   );
 }
--- src/components/WeatherSection/WeatherSection.tsx
+++ src/components/WeatherSection/WeatherSection.tsx
@@ -78,7 +78,7 @@ export default async function WeatherSection({
         hourly={weatherData.hourly}
         tempUnit={temp}
         selectedDay={day}
-        className="xl:col-start-2 xl:row-span-full max-h-[43rem] xl:max-h-[49rem]"
+        className="xl:col-start-2 xl:row-span-full contain-size"
       />
     </div>
   );
