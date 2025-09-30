c9c22ca8a913772f751019cd336e9b3f85db2ea8
Kamran Kiani <kiani.km@gmail.com>
2025-09-30 18:59:32 +0330

    Update WeatherSkeleton.tsx

--- src/components/WeatherSkeleton/WeatherSkeleton.tsx
+++ src/components/WeatherSkeleton/WeatherSkeleton.tsx
@@ -23,8 +23,9 @@ function WeatherSkeleton() {
   ];
   return (
     <div
-      role="status"
+      aria-busy="true"
       aria-live="polite"
+      role="alert"
       className="grid gap-8 xl:grid-cols-[2fr_1fr] xl:grid-rows-[auto_auto] xl:gap-8"
     >
       {/* Weather Info Card Skeleton */}
@@ -32,12 +33,13 @@ function WeatherSkeleton() {
         <div className="py-10 px-6 md:py-20 bg-neutral-800 rounded-20 flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0 animate-pulse relative">
           <div className="absolute inset-0 flex flex-col items-center justify-center bg-neutral-800 rounded-20">
             <LoadingDots />
-            <p className="text-neutral-300 mt-3 text-500" aria-live="assertive">
-              Loading...
-            </p>
+            <p className="text-neutral-300 mt-3 text-500">Loading...</p>
           </div>
 
-          <div className="text-center flex flex-col gap-3 md:text-start md:gap-6 max-w-[24rem]">
+          <div
+            aria-hidden="true"
+            className="text-center flex flex-col gap-3 md:text-start md:gap-6 max-w-[24rem]"
+          >
             <div className="h-7 bg-neutral-700 rounded-8 w-48"></div>
             <div className="h-6 bg-neutral-700 rounded-6 w-40"></div>
           </div>
@@ -48,7 +50,10 @@ function WeatherSkeleton() {
         </div>
 
         {/* Weather Details Cards Skeleton */}
-        <div className="grid gap-4 grid-cols-(--my-grid-cols-info) text-neutral-0">
+        <div
+          aria-hidden="true"
+          className="grid gap-4 grid-cols-(--my-grid-cols-info) text-neutral-0"
+        >
           {weatherDetailsLabel.map((label, index) => (
             <div
               key={index}
@@ -62,7 +67,10 @@ function WeatherSkeleton() {
       </div>
 
       {/* Daily Forecast Skeleton */}
-      <div className="xl:col-start-1 xl:row-start-2 self-end">
+      <div
+        aria-hidden="true"
+        className="xl:col-start-1 xl:row-start-2 self-end"
+      >
         <div className="h-6 bg-neutral-700 rounded-6 w-32 mb-5 animate-pulse"></div>
         <div className="grid gap-4 grid-cols-(--my-grid-cols-daily)">
           {Array.from({ length: 7 }).map((_, index) => (
@@ -82,7 +90,10 @@ function WeatherSkeleton() {
       </div>
 
       {/* Hourly Forecast Skeleton */}
-      <div className="py-5 px-4 bg-neutral-800 rounded-20 grid gap-4 xl:col-start-2 xl:row-span-full max-h-[43rem] animate-pulse">
+      <div
+        aria-hidden="true"
+        className="py-5 px-4 bg-neutral-800 rounded-20 grid gap-4 xl:col-start-2 xl:row-span-full max-h-[43rem] animate-pulse"
+      >
         <div className="flex items-center justify-between gap-2 flex-wrap">
           <div className="h-6 bg-neutral-700 rounded-6 w-32"></div>
           <div className="h-10 bg-neutral-700 rounded-8 w-32"></div>
